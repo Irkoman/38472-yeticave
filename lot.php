9de9c441e19a7a464bd3e822e87cad07f33f91bf
Irina Smirnova <smirnovapr@mail.ru>
2017-05-02 12:48:03 +0300

    Added correct links to lots and NotFoundPage

--- lot.php
+++ lot.php
@@ -1,27 +1,34 @@
 <?php
 require_once './functions.php';
+require_once './data.php';
 
-// ставки пользователей
-$bets = [
-  ['name' => 'Иван', 'price' => 11500, 'ts' => strtotime('-' . rand(1, 50) .' minute')],
-  ['name' => 'Константин', 'price' => 11000, 'ts' => strtotime('-' . rand(1, 18) .' hour')],
-  ['name' => 'Евгений', 'price' => 10500, 'ts' => strtotime('-' . rand(25, 50) .' hour')],
-  ['name' => 'Семён', 'price' => 10000, 'ts' => strtotime('last week')]
-];
+$id = $_GET['id'];
+
+if (!isset($lots[$id])) {
+  header("HTTP/1.1 404 Not Found");
+} else {
+  $lot = $lots[$id];
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="ru">
 <head>
   <meta charset="UTF-8">
-  <title>DC Ply Mens 2016/2017 Snowboard</title>
+  <title><?= $lot['title'] ?></title>
   <link href="css/normalize.min.css" rel="stylesheet">
   <link href="css/style.css" rel="stylesheet">
 </head>
 <body>
 
 <?= includeTemplate('templates/header.php') ?>
-<?= includeTemplate('templates/main-lot.php', ['bets' => $bets]) ?>
+
+<?php if (isset($lots[$id])): ?>
+<?= includeTemplate('templates/main-lot.php', ['lot' => $lot, 'bets' => $bets]) ?>
+<?php else: ?>
+<?= includeTemplate('templates/not-found.php') ?>
+<?php endif; ?>
+
 <?= includeTemplate('templates/footer.php') ?>
 
 </body>
--- templates/main-lot.php
+++ templates/main-lot.php
@@ -1,3 +1,7 @@
+<?php
+$lot = $data['lot'];
+?>
+
 <main>
   <nav class="nav">
     <ul class="nav__list container">
@@ -22,13 +26,13 @@
     </ul>
   </nav>
   <section class="lot-item container">
-    <h2>DC Ply Mens 2016/2017 Snowboard</h2>
+    <h2><?= $lot['title'] ?></h2>
     <div class="lot-item__content">
       <div class="lot-item__left">
         <div class="lot-item__image">
-          <img src="img/lot-image.jpg" width="730" height="548" alt="Сноуборд">
+          <img src=<?= $lot['url'] ?> width="730" height="548" alt="">
         </div>
-        <p class="lot-item__category">Категория: <span>Доски и лыжи</span></p>
+        <p class="lot-item__category">Категория: <span><?= $lot['category'] ?></span></p>
         <p class="lot-item__description">Легкий маневренный сноуборд, готовый дать жару в любом парке, растопив снег мощным щелчком и четкими дугами. Стекловолокно Bi-Ax, уложенное в двух направлениях, наделяет этот снаряд отличной гибкостью и отзывчивостью, а симметричная геометрия в сочетании с классическим прогибом кэмбер позволит уверенно держать высокие скорости. А если к концу катального дня сил совсем не останется, просто посмотрите на Вашу доску и улыбнитесь, крутая графика от Шона Кливера еще никого не оставляла равнодушным.</p>
       </div>
       <div class="lot-item__right">
@@ -39,7 +43,7 @@
           <div class="lot-item__cost-state">
             <div class="lot-item__rate">
               <span class="lot-item__amount">Текущая цена</span>
-              <span class="lot-item__cost">11 500</span>
+              <span class="lot-item__cost"><?= $lot['price'] ?></span>
             </div>
             <div class="lot-item__min-cost">
               Мин. ставка <span>12 000 р</span>
